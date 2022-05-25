4f8e6e9dae22f9dd163639db1b91540f099da066
Bruno <brunocfaraco@hotmail.com.br>
2022-05-25 17:13:43 -0300

    requisito 4 concluído

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const body = document.body
+
+function fillPixelBoard(number) {
+    for (let index = 0; index < number; index += 1) {
+        let pixelBlank = document.createElement('div');
+        pixelBlank.setAttribute('class', 'pixel');
+        document.getElementById('pixel-board').appendChild(pixelBlank);
+    }
+}
+
+fillPixelBoard(25);
